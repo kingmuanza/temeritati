a61750ec0e2319e744930ad07bbc75bfba4c51ca
unknown <muanza.kangudie@gmail.com>
2022-06-29 15:23:29 +0100

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'temeritati';
   theme = true;
+  url = "";
+  afficherMenu = true;
+
+  constructor(
+    private router: Router,
+    private location: Location
+  ) {
+    router.events.subscribe((val) => {
+      if(location.path() != ''){
+        this.url = location.path();
+      } else {
+        this.url = 'Home'
+      }
+
+      if (this.url.indexOf('/login') !== -1) {
+        this.afficherMenu = false;
+      } else {
+
+        this.afficherMenu = true;
+      }
+      console.log('url');
+      console.log(this.url);
+    });
+  }
+
+  logIn() {
+    this.router.navigate(['login']);
+  }
+  home() {
+    this.router.navigate(['/']);
+  }
 }
